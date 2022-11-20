d63fca871803607ec6082545a9f2f6315d3cee08
bilkn <bilkan.konus@gmail.com>
2022-11-20 18:17:55 +0300

    refactor: remove duplicated module declarations

--- libs/assetto-components/src/lib/components/MobileNavMenu/style.ts
+++ libs/assetto-components/src/lib/components/MobileNavMenu/style.ts
@@ -1,29 +1,35 @@
 import styled from '@emotion/styled';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
-export const MobileMenuContainer = styled('ul')(({ theme }) => `
+export const MobileMenuContainer = styled('ul')(
+  ({ theme }) => `
   background: #fff;
   border: 1px solid #e0e0e0;
   border-radius: 5px;
   display: flex;
   flex-direction: column;
   padding-left: 0;
-`);
+`
+);
 
-export const MobileMenuItem = styled('li')(({ theme }) => `
+export const MobileMenuItem = styled('li')(
+  ({ theme }) => `
   border-bottom: 1px solid #eee;
-  color: ${(props:any) => (props.active ? 'black' : 'red')};
+  color: ${(props: any) => (props.active ? 'black' : 'red')};
   text-transform: capitalize;
   list-style: none;
   &:hover {
     color: #0000009e;
   }
-`);
+`
+);
 
-export const MobileLink = styled(ReactRouterLink)(({ theme }) => `
+export const MobileLink = styled(ReactRouterLink)(
+  ({ theme }) => `
   color: 'yellow';
   font-size: 2rem;
-`);
+`
+);
 
 export const MobileMenuLink = styled(ReactRouterLink)`
   align-items: center;
--- libs/custom-theme/src/lib/custom-theme.ts
+++ libs/custom-theme/src/lib/custom-theme.ts
@@ -1,16 +1,16 @@
 import { createTheme, Shadows } from '@mui/material/styles';
 import { createBreakpoints } from '@mui/system';
 
-const breakpoints = createBreakpoints({})
+const breakpoints = createBreakpoints({});
 
-const customShadows = Array(25).fill('none') as Shadows
-customShadows[1] = "0px 4px 15px 0px #C9C9C940"
-customShadows[2] = "0px 1px 7px rgba(0, 0, 0, 0.3)"
+const customShadows = Array(25).fill('none') as Shadows;
+customShadows[1] = '0px 4px 15px 0px #C9C9C940';
+customShadows[2] = '0px 1px 7px rgba(0, 0, 0, 0.3)';
 
 const theme = createTheme({
   palette: {
     background: {
-      default:'#F7F8FA'
+      default: '#F7F8FA',
     },
     primary: {
       main: '#573BFF',
@@ -22,39 +22,37 @@ const theme = createTheme({
       main: '#00C3F9',
       light: '#29D1FF',
       dark: '#06B4E4',
-      darker: '#0EA5CF'
+      darker: '#0EA5CF',
     },
     warning: {
       main: '#DE3131',
       light: '#de3131cc',
-      lighter: '#de3131b3'
-    }
-    ,text: {
-      primary:'#283054',
-      secondary: '#8D8D8D'
+      lighter: '#de3131b3',
+    },
+    text: {
+      primary: '#283054',
+      secondary: '#8D8D8D',
     },
     grey: {
       200: '#EAEAEA',
       400: '#C9C9C9',
       700: '#A5A5A5',
-      900: '#6E6E6E'
+      900: '#6E6E6E',
     },
     extra: {
       0: '#4BB542',
       1: '#86C600',
-    }
+    },
   },
-  shadows:customShadows,
+  shadows: customShadows,
   shape: {
-    borderRadius: 10
+    borderRadius: 10,
   },
   typography: {
-    body1:{
-      fontSize: '1.6rem'
-    }
-  }
+    body1: {
+      fontSize: '1.6rem',
+    },
+  },
 });
 
-
-
-export default theme
+export default theme;
