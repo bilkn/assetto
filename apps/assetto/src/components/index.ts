f2617fc82d315ac8a7f958e482f6cc9b33ac0add
bilkn <bilkan.konus@gmail.com>
2022-12-02 22:57:48 +0300

    [AA-15] feat: add Total Balance Chart (WIP)

--- apps/assetto/src/components/index.ts
+++ apps/assetto/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as PriceCard } from './PriceCard';
 export { default as AssetCard } from './AssetCard';
 export { default as MobileAppBar } from './MobileAppBar';
 export { default as AssetModalCurrency } from './asset-modals/AssetModalCurrency';
+export { default as CustomChart } from './CustomChart';
